d2b3948a1eafe1b42782a84a4b4b48bd4aae2c32
PratikAwaik <pratikawaik125@gmail.com>
2023-12-07 19:11:39 +0530

    add epic fhir oauth authorize

--- src/App.tsx
+++ src/App.tsx
@@ -28,11 +28,25 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const redirect_uri =
+    process.env.NODE_ENV === "production"
+      ? "https://flexiple-assignment-pratikawaik.vercel.app"
+      : "http://localhost:3000";
+
+  const client_id = "eb3422dc-40d1-4722-8a79-4383e0d9c752";
+
   return (
     <main
       className="main flex items-center justify-center mx-auto max-w-6xl my-5 p-4"
       id="main"
     >
+      <a
+        href={`https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize?response_type=code&redirect_uri=${redirect_uri}&client_id=${client_id}&state=1234&scope=fhirUser Observation.read Observation.search`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Click
+      </a>
       <CommentsContainer allComments={storeComments} />
     </main>
   );
